f5bfd86127113777e130703ea512e60d4db03d07
baothi <baothi246@gmail.com>
2021-03-31 10:26:12 +0700

    đã xong phần login và Register

--- AppointmentScheduling/Helper/Helper.cs
+++ AppointmentScheduling/Helper/Helper.cs
@@ -11,7 +11,7 @@ namespace AppointmentScheduling.Utility
         public static string Admin = "Admin";
         public static string Patient = "Patient";
         public static string Doctor = "Doctor";
-        public static string appointmentAdded = "Appointment added successfully.";
+       /* public static string appointmentAdded = "Appointment added successfully.";
         public static string appointmentUpdated = "Appointment updated successfully.";
         public static string appointmentDeleted = "Appointment deleted successfully.";
         public static string appointmentExists = "Appointment for selected date and time already exists.";
@@ -22,9 +22,9 @@ namespace AppointmentScheduling.Utility
         public static string appointmentUpdatError = "Something went wront, Please try again.";
         public static string somethingWentWrong = "Something went wront, Please try again.";
         public static int success_code = 1;
-        public static int failure_code = 0;
+        public static int failure_code = 0;*/
 
-        public static List<SelectListItem> GetRolesForDropDown(bool isAdmin)
+        public static List<SelectListItem> GetRolesForDropDown()
         {
            /* if (isAdmin)
             {*/

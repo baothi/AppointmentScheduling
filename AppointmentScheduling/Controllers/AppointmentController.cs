eebe7f934483f262b24d413187d9255d6f5493f9
baothi <baothi246@gmail.com>
2021-04-01 14:12:10 +0700

    show moda ok

--- AppointmentScheduling/Controllers/AppointmentController.cs
+++ AppointmentScheduling/Controllers/AppointmentController.cs
@@ -18,6 +18,7 @@ namespace AppointmentScheduling.Controllers
         public IActionResult Index()
         {
             ViewBag.DoctorList = _appointmentService.GetDoctorList();
+            ViewBag.PatientList = _appointmentService.GetPatientList();
             return View();
         }
     }
--- AppointmentScheduling/Services/AppointmentService.cs
+++ AppointmentScheduling/Services/AppointmentService.cs
@@ -33,7 +33,17 @@ namespace AppointmentScheduling.Services
 
         public List<PatientVM> GetPatientList()
         {
-            throw new NotImplementedException();
+            var patients = (from user in _db.Users
+                            join userRoles in _db.UserRoles on user.Id equals userRoles.UserId
+                            join roles in _db.Roles.Where(x => x.Name == Helper.Patient) on userRoles.RoleId equals roles.Id
+                            select new PatientVM
+                            {
+                                Id = user.Id,
+                                Name = user.Name
+                            }
+                           ).ToList();
+
+            return patients;
         }
     }
 }
